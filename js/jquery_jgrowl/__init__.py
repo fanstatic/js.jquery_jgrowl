e0308c7bad5f877ce0363f58ac355f8bf7177dc2
Jan-Jaap Driessen <devnull@localhost>
2010-12-01 17:08:17 +0100

    Initial import.

---
+++ js/jquery_jgrowl/__init__.py
@@ -0,0 +1,12 @@
+from fanstatic import Library, ResourceInclusion
+
+from js.jquery import jquery
+
+library = Library('jquery_jgrowl', 'resources')
+
+jquery_jgrowl_css = ResourceInclusion(library, 'jquery.jgrowl.css')
+
+jquery_jgrowl = ResourceInclusion(library, 'jquery.jgrowl.js',
+    minified='jquery.jgrowl_minified.js',
+    depends=[jquery, jquery_jgrowl_css])
+
