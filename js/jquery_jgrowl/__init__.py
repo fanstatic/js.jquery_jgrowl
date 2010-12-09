b0072edbc4b26c5a9a0fc731110c1e24d34d705d
Jan-Jaap Driessen <devnull@localhost>
2010-12-09 12:15:54 +0100

    Fix name (minified <> minimized).

--- js/jquery_jgrowl/__init__.py
+++ js/jquery_jgrowl/__init__.py
@@ -7,6 +7,6 @@ library = Library('jquery_jgrowl', 'resources')
 jquery_jgrowl_css = ResourceInclusion(library, 'jquery.jgrowl.css')
 
 jquery_jgrowl = ResourceInclusion(library, 'jquery.jgrowl.js',
-    minified='jquery.jgrowl_minified.js',
+    minified='jquery.jgrowl_minimized.js',
     depends=[jquery, jquery_jgrowl_css])
 
